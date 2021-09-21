dcfea2e96bf9474bf3f34c8002f56db959563b5f
GabrielCorcelli <84634923+GabrielCorcelli@users.noreply.github.com>
2021-09-21 15:33:55 -0300

    Update veliocidade do carro.py

--- veliocidade do carro.py
+++ veliocidade do carro.py
@@ -1,5 +1,5 @@
 velocidade= float (input('Qual era a velocidade do carro?'))
-if velocidade >90:
+if velocidade >80:
     print('dirija com segurança')
     multa=(velocidade-80)*7
     print('voce foi multado o valor é $ {}'.format(multa))
